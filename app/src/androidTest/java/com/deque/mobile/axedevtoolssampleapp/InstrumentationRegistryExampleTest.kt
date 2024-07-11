8855a98c3ec7261b8c43d2da4f3e64d1fdfabe32
Andy Wyatt <andy.wyatt@deque.com>
2024-07-11 16:15:46 +0100

    chore: updates to gradle and fixes to tests

--- app/src/androidTest/java/com/deque/mobile/axedevtoolssampleapp/InstrumentationRegistryExampleTest.kt
+++ app/src/androidTest/java/com/deque/mobile/axedevtoolssampleapp/InstrumentationRegistryExampleTest.kt
@@ -1,10 +1,15 @@
 package com.deque.mobile.axedevtoolssampleapp
 
+import android.content.res.Resources
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
+import com.deque.axe.android.constants.AxeStatus
 import com.deque.mobile.axedevtoolssampleapp.test.BuildConfig
 import com.deque.mobile.devtools.AxeDevTools
-import org.junit.After
+import com.deque.mobile.devtools.ScanResultHandler
+import com.deque.mobile.devtools.testingconfigs.AxeDevToolsEspressoConfig
+import junit.framework.TestCase.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -15,52 +20,55 @@ class InstrumentationRegistryExampleTest {
     @JvmField
     val rule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
 
-
     private val axe = AxeDevTools()
 
     init {
         axe.loginWithApiKey(BuildConfig.AXE_DEVTOOLS_APIKEY)
-    }
+         }
 
     @Before
     fun setupAxeDevTools() {
+        axe.tagScanAs(setOf("myAxeEspressoTest"))
+        axe.setTestingConfig(
+            AxeDevToolsEspressoConfig(IdlingRegistry.getInstance())
+        )
         axe.setInstrumentation(InstrumentationRegistry.getInstrumentation())
     }
 
     @Test
-    fun test_using_registry() {
-        axe.setInstrumentation(InstrumentationRegistry.getInstrumentation())
-        val scanResultHandler = axe.scan()
-//      1. Upload it to the dashboard
-        scanResultHandler?.uploadToDashboard()
-
-//      2. Using the results in your test suite
-//        var passes = 0
-//        var fails = 0
-//        var incomplete = 0
-//        val result: AxeResult? = scanResultHandler?.serializedResult
-//        result?.axeRuleResults?.forEach { ruleResult ->
-//            when (ruleResult.status) {
-//                AxeStatus.PASS -> {
-//                    passes++
-//                }
-//
-//                AxeStatus.FAIL -> {
-//                    fails++
-//                }
-//
-//                AxeStatus.INCOMPLETE -> {
-//                    incomplete++
-//                }
-//            }
-//        }
-//        assertEquals(6, fails)
-
-//        3. Save the result JSON to a local file for later use
-        scanResultHandler?.saveResultToLocalStorage("axe")
+    fun test_using_activity() {
+        rule.scenario.onActivity {
+            val scanResultHandler = axe.scan(it)
+            processAccessibilityResults(scanResultHandler)
+        }
     }
 
-    @After
+    private fun processAccessibilityResults(scanResultHandler: ScanResultHandler?) {
+        var passes = 0
+        var fails = 0
+        var incomplete = 0
+        val result: com.deque.axe.android.AxeResult? = scanResultHandler?.serializedResult
+        scanResultHandler?.uploadToDashboard()
+        scanResultHandler?.saveResultToLocalStorage("axe")
+
+        result?.axeRuleResults?.forEach { ruleResult ->
+            when (ruleResult.status) {
+                AxeStatus.PASS -> {
+                    passes++
+                }
+
+                AxeStatus.FAIL -> {
+                    fails++
+                }
+
+                AxeStatus.INCOMPLETE -> {
+                    incomplete++
+                }
+            }
+        }
+        assertEquals(4, fails)
+    }
+
     fun tearDown() {
         axe.tearDown()
     }
