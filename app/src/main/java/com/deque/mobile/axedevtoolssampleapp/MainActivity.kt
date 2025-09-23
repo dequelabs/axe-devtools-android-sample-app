a58c55f3c74ea6b1ab47d501ad14941eb4ef7e4e
patrickbodell <patrick.bodell@deque.com>
2025-09-23 15:21:54 -0600

    Implement nav graph

--- app/src/main/java/com/deque/mobile/axedevtoolssampleapp/MainActivity.kt
+++ app/src/main/java/com/deque/mobile/axedevtoolssampleapp/MainActivity.kt
@@ -1,40 +1,56 @@
 package com.deque.mobile.axedevtoolssampleapp
 
 import android.os.Bundle
-import android.view.View.GONE
-import android.view.View.VISIBLE
-import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
+import androidx.navigation.createGraph
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.fragment
+import com.deque.mobile.axedevtoolssampleapp.ui.Cart
+import com.deque.mobile.axedevtoolssampleapp.ui.Catalog
+import com.deque.mobile.axedevtoolssampleapp.ui.Menu
 import com.google.android.material.navigation.NavigationBarView
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.collectLatest
 
 class MainActivity : AppCompatActivity() {
-    private val nextFragment = MutableStateFlow<Fragment>(FragmentCatalog())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        launchNextFragment(nextFragment.value)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        navController.graph = navController.createGraph(
+            startDestination = Catalog
+        ) {
+            fragment<FragmentCatalog, Catalog> {
+                label = "Catalog"
+            }
+
+            fragment<FragmentCart, Cart> {
+                label = "Cart"
+            }
+
+            fragment<FragmentMenu, Menu> {
+                label = "Menu"
+            }
+        }
+
 
         val bottomNav: NavigationBarView = findViewById(R.id.bottom_nav_bar)
 
         bottomNav.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.catalog -> {
-                    nextFragment.value = FragmentCatalog()
+                    navController.navigate(Catalog)
                     true
                 }
 
                 R.id.cart -> {
-                    nextFragment.value = FragmentCart()
+                    navController.navigate(Cart)
                     true
                 }
 
                 R.id.menu -> {
-                    nextFragment.value = FragmentMenu()
+                    navController.navigate(Menu)
                     true
                 }
 
@@ -49,60 +65,5 @@ class MainActivity : AppCompatActivity() {
                 R.id.catalog, R.id.cart, R.id.menu -> {}
             }
         }
-
-        lifecycleScope.launchWhenStarted {
-            nextFragment.collectLatest {
-                launchNextFragment(it)
-            }
-        }
-    }
-
-    private fun launchNextFragment(fragment: Fragment) {
-        val bottomNavBar: NavigationBarView = findViewById(R.id.bottom_nav_bar)
-        val startFrame: FrameLayout = findViewById(R.id.start_frame)
-        val mainFrame: FrameLayout = findViewById(R.id.main_frame)
-
-        carouselStop()
-
-        if (fragment is FragmentCarousel) {
-            bottomNavBar.visibility = GONE
-            startFrame.visibility = VISIBLE
-            mainFrame.visibility = GONE
-
-            mainFrame.removeAllViews()
-            supportFragmentManager.beginTransaction().replace(R.id.start_frame, fragment).commit()
-        } else {
-            bottomNavBar.visibility = VISIBLE
-            startFrame.visibility = GONE
-            mainFrame.visibility = VISIBLE
-
-            startFrame.removeAllViews()
-            supportFragmentManager.beginTransaction().replace(R.id.main_frame, fragment)
-                .addToBackStack(fragment::class.simpleName).commit()
-        }
-    }
-
-    override fun onBackPressed() {
-        val bottomNav: NavigationBarView = findViewById(R.id.bottom_nav_bar)
-        val nextPop =
-            supportFragmentManager.getBackStackEntryAt(supportFragmentManager.backStackEntryCount - 2)
-
-        when (nextPop.name) {
-            FragmentCatalog::class.simpleName -> bottomNav.selectedItemId = R.id.catalog
-            FragmentCart::class.simpleName -> bottomNav.selectedItemId = R.id.cart
-            FragmentMenu::class.simpleName -> bottomNav.selectedItemId = R.id.menu
-        }
-
-        supportFragmentManager.popBackStack()
-        super.onBackPressed()
-    }
-}
-
-val isRunningTest: Boolean by lazy {
-    try {
-        Class.forName("androidx.test.espresso.Espresso")
-        true
-    } catch (e: ClassNotFoundException) {
-        false
     }
 }
---
+++ app/src/main/java/com/deque/mobile/axedevtoolssampleapp/ui/Destinations.kt
@@ -0,0 +1,7 @@
+package com.deque.mobile.axedevtoolssampleapp.ui
+
+import kotlinx.serialization.Serializable
+
+@Serializable data object Catalog
+@Serializable data object Cart
+@Serializable data object Menu
