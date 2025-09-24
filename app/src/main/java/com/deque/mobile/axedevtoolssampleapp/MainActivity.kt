67a1f54fb78b99d5871df6fc68bbec046bcd2a12
patrickbodell <patrick.bodell@deque.com>
2025-09-24 12:25:49 -0600

    Opted for navigation resource for cleaner implementation

--- app/src/main/java/com/deque/mobile/axedevtoolssampleapp/MainActivity.kt
+++ app/src/main/java/com/deque/mobile/axedevtoolssampleapp/MainActivity.kt
@@ -2,10 +2,8 @@ package com.deque.mobile.axedevtoolssampleapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.createGraph
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.fragment
-import com.deque.mobile.axedevtoolssampleapp.ui.Destinations
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
@@ -16,54 +14,7 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
-        navController.graph = navController.createGraph(
-            startDestination = Destinations.Catalog
-        ) {
-            fragment<FragmentCatalog, Destinations.Catalog> {
-                label = "Catalog"
-            }
-
-            fragment<FragmentCart, Destinations.Cart> {
-                label = "Cart"
-            }
-
-            fragment<FragmentMenu, Destinations.Menu> {
-                label = "Menu"
-            }
-        }
-
-
         val bottomNav: NavigationBarView = findViewById(R.id.bottom_nav_bar)
-
-        bottomNav.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.catalog -> {
-                    navController.navigate(Destinations.Catalog)
-                    true
-                }
-
-                R.id.cart -> {
-                    navController.navigate(Destinations.Cart)
-                    true
-                }
-
-                R.id.menu -> {
-                    navController.navigate(Destinations.Menu)
-                    true
-                }
-
-                else -> {
-                    throw IllegalArgumentException("That nav option does not exist.")
-                }
-            }
-        }
-
-        bottomNav.setOnItemReselectedListener { item ->
-            when (item.itemId) {
-                R.id.catalog, R.id.cart, R.id.menu -> {}
-            }
-        }
-
+        bottomNav.setupWithNavController(navController)
     }
-
 }
