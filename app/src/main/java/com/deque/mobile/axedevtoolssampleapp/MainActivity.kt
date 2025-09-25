a3c58b3fa73890f66e3e165d46d7d5d9962d217d
pat-deque <92315936+pat-deque@users.noreply.github.com>
2025-09-25 10:41:17 -0600

    chore: Update Sample App To Keep App State After Rotation (#58)
    * Upgrade AGP, Kotlin, and targetSDK versions
    
    * Implement nav graph

--- app/src/main/java/com/deque/mobile/axedevtoolssampleapp/MainActivity.kt
+++ app/src/main/java/com/deque/mobile/axedevtoolssampleapp/MainActivity.kt
@@ -1,108 +1,20 @@
 package com.deque.mobile.axedevtoolssampleapp
 
 import android.os.Bundle
-import android.view.View.GONE
-import android.view.View.VISIBLE
-import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import com.google.android.material.navigation.NavigationBarView
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.collectLatest
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private val nextFragment = MutableStateFlow<Fragment>(FragmentCatalog())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        launchNextFragment(nextFragment.value)
-
-        val bottomNav: NavigationBarView = findViewById(R.id.bottom_nav_bar)
-
-        bottomNav.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.catalog -> {
-                    nextFragment.value = FragmentCatalog()
-                    true
-                }
-
-                R.id.cart -> {
-                    nextFragment.value = FragmentCart()
-                    true
-                }
-
-                R.id.menu -> {
-                    nextFragment.value = FragmentMenu()
-                    true
-                }
-
-                else -> {
-                    throw IllegalArgumentException("That nav option does not exist.")
-                }
-            }
-        }
-
-        bottomNav.setOnItemReselectedListener { item ->
-            when (item.itemId) {
-                R.id.catalog, R.id.cart, R.id.menu -> {}
-            }
-        }
-
-        lifecycleScope.launchWhenStarted {
-            nextFragment.collectLatest {
-                launchNextFragment(it)
-            }
-        }
-    }
-
-    private fun launchNextFragment(fragment: Fragment) {
-        val bottomNavBar: NavigationBarView = findViewById(R.id.bottom_nav_bar)
-        val startFrame: FrameLayout = findViewById(R.id.start_frame)
-        val mainFrame: FrameLayout = findViewById(R.id.main_frame)
-
-        carouselStop()
-
-        if (fragment is FragmentCarousel) {
-            bottomNavBar.visibility = GONE
-            startFrame.visibility = VISIBLE
-            mainFrame.visibility = GONE
-
-            mainFrame.removeAllViews()
-            supportFragmentManager.beginTransaction().replace(R.id.start_frame, fragment).commit()
-        } else {
-            bottomNavBar.visibility = VISIBLE
-            startFrame.visibility = GONE
-            mainFrame.visibility = VISIBLE
-
-            startFrame.removeAllViews()
-            supportFragmentManager.beginTransaction().replace(R.id.main_frame, fragment)
-                .addToBackStack(fragment::class.simpleName).commit()
-        }
-    }
-
-    override fun onBackPressed() {
-        val bottomNav: NavigationBarView = findViewById(R.id.bottom_nav_bar)
-        val nextPop =
-            supportFragmentManager.getBackStackEntryAt(supportFragmentManager.backStackEntryCount - 2)
-
-        when (nextPop.name) {
-            FragmentCatalog::class.simpleName -> bottomNav.selectedItemId = R.id.catalog
-            FragmentCart::class.simpleName -> bottomNav.selectedItemId = R.id.cart
-            FragmentMenu::class.simpleName -> bottomNav.selectedItemId = R.id.menu
-        }
-
-        supportFragmentManager.popBackStack()
-        super.onBackPressed()
-    }
-}
-
-val isRunningTest: Boolean by lazy {
-    try {
-        Class.forName("androidx.test.espresso.Espresso")
-        true
-    } catch (e: ClassNotFoundException) {
-        false
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNav: BottomNavigationView = findViewById(R.id.bottom_nav_bar)
+        setupWithNavController(bottomNav, navController)
     }
 }
